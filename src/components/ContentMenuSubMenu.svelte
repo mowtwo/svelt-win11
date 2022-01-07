db12b7eaee5c152e82ecd11da1906c0bc58a74ac
mowtwo <1046099511@qq.com>
2022-01-07 20:22:30 +0800

    子菜单抽离组件

---
+++ src/components/ContentMenuSubMenu.svelte
@@ -0,0 +1,30 @@
+<script lang="ts" context="module">
+</script>
+
+<script lang="ts">
+  import { on } from "events";
+
+  import { onMount } from "svelte";
+
+  export let align: "left" | "right" = "left";
+  export let offset = 0;
+  export let outWidth = 0;
+  $: offsetStyle = `${align}:${offset}px`;
+  let menuThis: HTMLDivElement;
+  onMount(() => {
+    const { width } = menuThis.getBoundingClientRect();
+    outWidth = width;
+  });
+</script>
+
+<div class="menu" style={offsetStyle} bind:this={menuThis}>
+  <slot />
+</div>
+
+<style lang="scss">
+  .menu {
+    position: absolute;
+    z-index: 2;
+    top: 0;
+  }
+</style>
