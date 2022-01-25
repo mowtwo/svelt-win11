7bf27ce4eef29cec66a9393e999165193015e5ce
向越可 <bombless@126.com>
2022-01-25 17:14:17 +0800

    图标增加待选中效果

--- src/components/Shortcut.svelte
+++ src/components/Shortcut.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-  export let icon, name, position
+  export let icon, name, position, title
   let style = `left: ${position[0]}px; top: ${position[1]}px`
 </script>
 
-<div class="dskApp container" {style}>
+<div class="dskApp container" {style} title={title}>
   <img width="36" src="{icon}" alt="icon" />
   <div class="appName">{name}</div>
 </div>
 
 <style lang="scss">
-
+  img {
+    margin-top: 16px;
+  }
   .container {
     position: fixed;
-    z-index: 999999999999;
+    z-index: 1;
     width: 120px;
     min-height: 100px;
     text-align: center;
@@ -23,8 +25,12 @@
     color: #fafafa;
     margin-top: 4px;
     text-shadow: 0 0 4px rgba(0, 0, 0, 0.6);
+    cursor: default;
   }
   .dskApp {
     font-size: 12.8px;
+    &:hover {
+      background-color: rgba($color: #fff, $alpha: 0.6);
+    }
   }
 </style>
--- src/lib/ShortcutSystem.svelte
+++ src/lib/ShortcutSystem.svelte
@@ -2,6 +2,6 @@
     import Shortcut from "../components/Shortcut.svelte";
     import shortcutCollection from "../store/desktopShortcuts";
 </script>
-{#each shortcutCollection as {icon, name, position}}
-    <Shortcut {icon} {name} {position}></Shortcut>
+{#each shortcutCollection as {icon, name, position, title}}
+    <Shortcut {icon} {name} {position} {title}></Shortcut>
 {/each}
